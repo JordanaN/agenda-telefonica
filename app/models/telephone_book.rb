d635e7e7840ddb9b6a06a12ef5f07a2f7f3f34da
JordanaN <jordanasnogueira@gmail.com>
2019-07-22 23:19:03 -0300

    remove antiga estrutura do projeto

--- app/models/telephone_book.rb
+++
@@ -1,3 +0,0 @@
-class TelephoneBook < ActiveRecord::Base
-  belongs_to :user
-end
--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-User.create(first_name: 'João', last_name: 'Souza', email: 'joao@example.com', password_digest: '123456')
-User.create(first_name: 'Jose', last_name: 'Silva', email: 'jose@example.com', password_digest: '123456')
-User.create(first_name: 'Maria', last_name: 'Santos', email: 'maria@example.com', password_digest: '123456')
-User.create(first_name: 'Antonio', last_name: 'Andrade', email: 'antonio@example.com', password_digest: '123456')
-
-TelephoneBook.create(phone_number: '3454-3368', cell_phone_number: '98218-4758', user_id: 1)
-TelephoneBook.create(phone_number: '3454-3368', cell_phone_number: '98218-4758', user_id: 2)
-TelephoneBook.create(phone_number: '3454-3368', cell_phone_number: '98218-4758', user_id: 3)
-TelephoneBook.create(phone_number: '3454-3368', cell_phone_number: '98218-4758', user_id: 4)
